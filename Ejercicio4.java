ee2a239fe0a463958e45b7a7dc56fbf19ea76163
Ulises <ulimarian2000@gmail.com>
2021-10-07 10:45:26 -0300

    Alumnos separados en clases distintas

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -1,15 +1,34 @@
 package com.informatorio;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Ejercicio4 {
     public static void main(String[] args){
-        String [] estudiantes = {"Lourdes", "Juana","Pedro","Juan","Marcelo","Maria","Tatiana","Judas","Tomas","Mariano","Pablo","Aldana"};
 
-        for (String i: estudiantes) {
-            System.out.println(i);
+        System.out.println("Los estudiantes de la clase numero 1 son: ");
+        indPosicion(0,4);
+        System.out.println("Los estudiantes de la clase numero 2 son: ");
+        indPosicion(4,8);
+        System.out.println("Los estudiantes de la clase numero 3 son: ");
+        indPosicion(8,12);
+    }
+    static void indPosicion (int inicio,int fin){
+        ArrayList estudiante = new ArrayList();
+        estudiante.add("Juan");
+        estudiante.add("Osvaldo");
+        estudiante.add("Juana");
+        estudiante.add("Sebastian");
+        estudiante.add("Lourdes");
+        estudiante.add("Aldana");
+        estudiante.add("Marcelo");
+        estudiante.add("Maria");
+        estudiante.add("Macarena");
+        estudiante.add("Braian");
+        estudiante.add("Lorena");
+        estudiante.add("Jose");
+        for (int i = inicio; i < fin; i++) {
+            System.out.println(estudiante.get(i));
         }
-        String [] newArray = estudiantes.split()
+
     }
 }
