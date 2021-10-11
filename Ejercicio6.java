e3db09deb19c4d850011d86108be5ca6b31c8084
Ulises <ulimarian2000@gmail.com>
2021-10-11 20:06:04 -0300

    Ejercicios incompletos 6 y 7

--- Ejercicio6.java
+++ Ejercicio6.java
@@ -1,29 +1,56 @@
 package com.informatorio;
 
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
 
 public class Ejercicio6 {
-    public static void main(String[]args){
+//Completar...
 
-        HashSet<String> nombreApellido = new HashSet<>();
-        HashSet<Integer> dNI = new HashSet<>();
-        HashSet<Integer> horasTrabajadas = new HashSet<>();
-        HashSet<Integer> valorPorHora = new HashSet<>();
-        HashMap<String, Integer> trabajado = new HashMap();
+    public static void main(String[] args)  {
+        List<String> nombreApellido = new ArrayList<>();
+        List<Integer> dni = new ArrayList<>();
+        List<Integer> horasTrabajadas = new ArrayList<>();
+        List<Integer> valorPorHora = new ArrayList<>();
+        List<Integer> totales = new ArrayList<>();
 
-        nombreApellido.add("Juan perez");                dNI.add(1234);         horasTrabajadas.add(6);
-        nombreApellido.add("Mariana Swamp");             dNI.add(2545);         horasTrabajadas.add(7);
-        nombreApellido.add("Katherin Cocina");           dNI.add(4567);         horasTrabajadas.add(8);
-        nombreApellido.add("Pedro Cordero");             dNI.add(7890);         horasTrabajadas.add(4);
-        nombreApellido.add("Valentina VillaBlanca");     dNI.add(3698);         horasTrabajadas.add(5);
-
+        horasTrabajadas.add(6);           nombreApellido.add("Valeria");          dni.add(2565484);
+        horasTrabajadas.add(7);           nombreApellido.add("Juan");             dni.add(3215646);
+        horasTrabajadas.add(8);           nombreApellido.add("Valentina");        dni.add(8798654);
+        horasTrabajadas.add(4);           nombreApellido.add("Viviana");          dni.add(3246548);
+        horasTrabajadas.add(5);           nombreApellido.add("Victor");           dni.add(3454887);
         valorPorHora.add(350);
         valorPorHora.add(345);
         valorPorHora.add(550);
         valorPorHora.add(600);
         valorPorHora.add(320);
 
-       
+        for (int i = 0; i < horasTrabajadas.size(); i++) {
+            //aquí tomo las horas trabajadas y se lo paso en multiplicacion al valor por hora
+            int hs = horasTrabajadas.get(i) * valorPorHora.get(i);
+            totales.add(hs);
+        }
+        int totalesHs = 0;
+        for (Integer hs : totales) {
+            totalesHs += hs;
+
+        }
+
+        Set<List<String>> nombre = new HashSet<>();
+        nombre.add(nombreApellido);
+        for (String s: nombreApellido) {
+            System.out.println(s);
+        }
+
+        Set<List<Integer>> allJunto = new HashSet<>();
+        allJunto.add(dni);
+        allJunto.add(horasTrabajadas);
+        allJunto.add(valorPorHora);
+        allJunto.add(totales);
+
+        for (List<Integer> num : allJunto) {
+            System.out.println(num);
+        }
+        HashMap<Set<List<String>>, Set<List<Integer>>> theLista = new HashMap<>();
+        theLista.put(nombre,allJunto);
+        System.out.println(theLista);
     }
 }
--- Ejercicio7.java
+++ Ejercicio7.java
@@ -1,42 +1,42 @@
 package com.informatorio;
 
-import javax.lang.model.type.ErrorType;
+import java.util.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
+//Terminar los detalles...
 public class Ejercicio7 {
-    public static void main(String[]args){
-
+    public static void main(String args[]){
         int num1;
         int num2;
-        List<Integer> numTotal = new ArrayList<>();
+
         Scanner numIngresado = new Scanner(System.in);
-        System.out.println("Ingrese su primer numero, que sea del 2 al 10: ");
+        List<Integer> numTotal = new ArrayList<>();
+        System.out.println("Ingrese su primer numero, que sea del menor: ");
         num1 = numIngresado.nextInt();
         numIngresado.nextLine();
         System.out.println("Ingrese su segundo numero, el más grande: ");
         num2 = numIngresado.nextInt();
         numIngresado.nextLine();
-        int num3 = (num2 + 1);
 
-            if (num1 == 5 || num1 == 7 || num1 == 1) {
-                System.out.println("Numero invalido, ingrese un numero multiplo de 2 o 3");
-            } else {
-                int calculo = num1 % 2;
-
-                if (calculo == 0) {
-                    for (int i = num1; i < num3; i++) {
-                        System.out.println(i + ". Fizz");
-                    }
-                } else if (calculo == 1) {
-                    for (int i = num1; i < num3; i++) {
-                        System.out.println(i + ". Buzz");
-                    }
-                }
+        if(num1%2==0 && num1%3 == 0){
+            for (int i = num1; i < num2; i++) {
+                System.out.println(i + ". FizzBuzz");
             }
-
-
+        }
+        else if(num1%3 ==0){
+            for (int i = num1; i < num2; i++) {
+                System.out.println(i + ". Buzz");
+            }
+        }
+        else if(num1%2 == 0 ){
+            for (int i = num1; i < num2; i++){
+                System.out.println(i + ". Fizz");
+            }
+        }
+        else {
+            System.out.println("Lo siento, pero no se encuentra ese numero...");
+        }
         }
     }
 
