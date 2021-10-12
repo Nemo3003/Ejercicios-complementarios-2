b6ed22fd6049e11305ad2713a796c66e932b4e99
Ulises <ulimarian2000@gmail.com>
2021-10-12 19:49:53 -0300

    ya listo el 7!

--- Ejercicio7.java
+++ Ejercicio7.java
@@ -3,10 +3,10 @@ package com.informatorio;
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Ejercicio7 {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         int num1;
         int num2;
-        ArrayList sumaTotal = new ArrayList();
+        ArrayList<String> sumaTotal = new ArrayList();
         Scanner numIngresado = new Scanner(System.in);
         System.out.println("Ingrese su primer numero, que sea del menor: ");
         num1 = numIngresado.nextInt();
@@ -15,28 +15,23 @@ public class Ejercicio7 {
         num2 = numIngresado.nextInt();
         numIngresado.nextLine();
 
-        for (int i = num1; i < num2; i++){
-            if(num1%2==0 && num1%3 == 0){
-                    sumaTotal.add(i + ". FizzBuzz");
-            }
-            else if (num1%3 == 0){
-                sumaTotal.add(i  + ". Buzz");
-            }
-            else if(num1%2==0){
-                sumaTotal.add(i  +". Fizz");
-            }
-            else{
+        for (int i = num1; i < num2; i++) {
+            if (num1 % 2 == 0 && num1 % 3 == 0) {
+                sumaTotal.add(i + ". FizzBuzz");
+            } else if (num1 % 3 == 0) {
+                sumaTotal.add(i + ". Buzz");
+            } else if (num1 % 2 == 0) {
+                sumaTotal.add(i + ". Fizz");
+            } else {
                 System.out.println("Numero no multiplo de 2 ni de 3");
-            }
-            for (int j = 0; j < sumaTotal.size(); j++) {
-                System.out.println(sumaTotal.get(j));
-
             }
 
         }
+        for (int j = 0; j < sumaTotal.size(); j++) {
+            System.out.println(sumaTotal.get(j));
 
-    }
-}
+        }
+    }}
 
 
 
